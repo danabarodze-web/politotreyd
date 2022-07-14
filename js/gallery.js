6c3210b5de089a3b5b202c1e3bb5ef2ddff4a264
danabarodze-web <85407711+danabarodze-web@users.noreply.github.com>
2022-07-14 11:39:04 +0300

    Add files via upload

---
+++ js/gallery.js
@@ -0,0 +1,209 @@
+$(document).ready(function () {
+    
+    lightGallery(document.getElementById('hash'), {
+        plugins: [lgZoom, lgThumbnail],
+        licenseKey: 'your_license_key',
+        speed: 500,
+    });
+    lightGallery(document.getElementById('hash2'), {
+        plugins: [lgZoom, lgThumbnail],
+        licenseKey: 'your_license_key',
+        speed: 500,
+    });
+    lightGallery(document.getElementById('hash3'), {
+        plugins: [lgZoom, lgThumbnail],
+        licenseKey: 'your_license_key',
+        speed: 500,
+    });
+    lightGallery(document.getElementById('hash4'), {
+        plugins: [lgZoom, lgThumbnail],
+        licenseKey: 'your_license_key',
+        speed: 500,
+    });
+    lightGallery(document.getElementById('hash5'), {
+        plugins: [lgZoom, lgThumbnail],
+        licenseKey: 'your_license_key',
+        speed: 500,
+    });
+    lightGallery(document.getElementById('hash6'), {
+        plugins: [lgZoom, lgThumbnail],
+        licenseKey: 'your_license_key',
+        speed: 500,
+    });
+    lightGallery(document.getElementById('hash7'), {
+        plugins: [lgZoom, lgThumbnail],
+        licenseKey: 'your_license_key',
+        speed: 500,
+    });
+    lightGallery(document.getElementById('hash8'), {
+        plugins: [lgZoom, lgThumbnail],
+        licenseKey: 'your_license_key',
+        speed: 500,
+    });
+    lightGallery(document.getElementById('hash9'), {
+        plugins: [lgZoom, lgThumbnail],
+        licenseKey: 'your_license_key',
+        speed: 500,
+    });
+    lightGallery(document.getElementById('hash10'), {
+        plugins: [lgZoom, lgThumbnail],
+        licenseKey: 'your_license_key',
+        speed: 500,
+    });
+    lightGallery(document.getElementById('hash11'), {
+        plugins: [lgZoom, lgThumbnail],
+        licenseKey: 'your_license_key',
+        speed: 500,
+    });
+    lightGallery(document.getElementById('hash12'), {
+        plugins: [lgZoom, lgThumbnail],
+        licenseKey: 'your_license_key',
+        speed: 500,
+    });
+    lightGallery(document.getElementById('hash13'), {
+        plugins: [lgZoom, lgThumbnail],
+        licenseKey: 'your_license_key',
+        speed: 500,
+    });
+    lightGallery(document.getElementById('hash14'), {
+        plugins: [lgZoom, lgThumbnail],
+        licenseKey: 'your_license_key',
+        speed: 500,
+    });
+    lightGallery(document.getElementById('hash15'), {
+        plugins: [lgZoom, lgThumbnail],
+        licenseKey: 'your_license_key',
+        speed: 500,
+    });
+    lightGallery(document.getElementById('hash16'), {
+        plugins: [lgZoom, lgThumbnail],
+        licenseKey: 'your_license_key',
+        speed: 500,
+    });
+    lightGallery(document.getElementById('hash17'), {
+        plugins: [lgZoom, lgThumbnail],
+        licenseKey: 'your_license_key',
+        speed: 500,
+    });
+    lightGallery(document.getElementById('hash18'), {
+        plugins: [lgZoom, lgThumbnail],
+        licenseKey: 'your_license_key',
+        speed: 500,
+    });
+    lightGallery(document.getElementById('hash19'), {
+        plugins: [lgZoom, lgThumbnail],
+        licenseKey: 'your_license_key',
+        speed: 500,
+    });
+    lightGallery(document.getElementById('hash20'), {
+        plugins: [lgZoom, lgThumbnail],
+        licenseKey: 'your_license_key',
+        speed: 500,
+    });
+    lightGallery(document.getElementById('hash21'), {
+        plugins: [lgZoom, lgThumbnail],
+        licenseKey: 'your_license_key',
+        speed: 500,
+    });
+    lightGallery(document.getElementById('hash22'), {
+        plugins: [lgZoom, lgThumbnail],
+        licenseKey: 'your_license_key',
+        speed: 500,
+    });
+    lightGallery(document.getElementById('hash23'), {
+        plugins: [lgZoom, lgThumbnail],
+        licenseKey: 'your_license_key',
+        speed: 500,
+    });
+    lightGallery(document.getElementById('hash24'), {
+        plugins: [lgZoom, lgThumbnail],
+        licenseKey: 'your_license_key',
+        speed: 500,
+    });
+    lightGallery(document.getElementById('hash25'), {
+        plugins: [lgZoom, lgThumbnail],
+        licenseKey: 'your_license_key',
+        speed: 500,
+    });
+    lightGallery(document.getElementById('hash26'), {
+        plugins: [lgZoom, lgThumbnail],
+        licenseKey: 'your_license_key',
+        speed: 500,
+    });
+    lightGallery(document.getElementById('hash27'), {
+        plugins: [lgZoom, lgThumbnail],
+        licenseKey: 'your_license_key',
+        speed: 500,
+    });
+    lightGallery(document.getElementById('hash28'), {
+        plugins: [lgZoom, lgThumbnail],
+        licenseKey: 'your_license_key',
+        speed: 500,
+    });
+    lightGallery(document.getElementById('hash29'), {
+        plugins: [lgZoom, lgThumbnail],
+        licenseKey: 'your_license_key',
+        speed: 500,
+    });
+    lightGallery(document.getElementById('hash30'), {
+        plugins: [lgZoom, lgThumbnail],
+        licenseKey: 'your_license_key',
+        speed: 500,
+    });
+    lightGallery(document.getElementById('hash31'), {
+        plugins: [lgZoom, lgThumbnail],
+        licenseKey: 'your_license_key',
+        speed: 500,
+    });
+    lightGallery(document.getElementById('hash32'), {
+        plugins: [lgZoom, lgThumbnail],
+        licenseKey: 'your_license_key',
+        speed: 500,
+    });
+    lightGallery(document.getElementById('hash33'), {
+        plugins: [lgZoom, lgThumbnail],
+        licenseKey: 'your_license_key',
+        speed: 500,
+    });
+    lightGallery(document.getElementById('hash34'), {
+        plugins: [lgZoom, lgThumbnail],
+        licenseKey: 'your_license_key',
+        speed: 500,
+    });
+    lightGallery(document.getElementById('hash35'), {
+        plugins: [lgZoom, lgThumbnail],
+        licenseKey: 'your_license_key',
+        speed: 500,
+    });
+    lightGallery(document.getElementById('hash36'), {
+        plugins: [lgZoom, lgThumbnail],
+        licenseKey: 'your_license_key',
+        speed: 500,
+    });
+    lightGallery(document.getElementById('hash37'), {
+        plugins: [lgZoom, lgThumbnail],
+        licenseKey: 'your_license_key',
+        speed: 500,
+    });
+    lightGallery(document.getElementById('hash38'), {
+        plugins: [lgZoom, lgThumbnail],
+        licenseKey: 'your_license_key',
+        speed: 500,
+    });
+    lightGallery(document.getElementById('hash39'), {
+        plugins: [lgZoom, lgThumbnail],
+        licenseKey: 'your_license_key',
+        speed: 500,
+    });
+    lightGallery(document.getElementById('hash40'), {
+        plugins: [lgZoom, lgThumbnail],
+        licenseKey: 'your_license_key',
+        speed: 500,
+    });
+    lightGallery(document.getElementById('hash41'), {
+        plugins: [lgZoom, lgThumbnail],
+        licenseKey: 'your_license_key',
+        speed: 500,
+    });
+
+})
